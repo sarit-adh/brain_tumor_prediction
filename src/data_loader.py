3c7d6145432d8d26f215c5850357da5a3eb70b38
sarit-adh <sarit.adhikari@gmail.com>
2024-09-07 20:04:55 -0500

    added code for loading data

---
+++ src/data_loader.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        image = cv2.imread(os.path.join(folder, filename))
+        if image is None:
+            print(f"Image {filename} could not be loaded!!")
+        else:
+            image = cv2.resize(image, (128, 128))
+            images.append(image)
+    return images
+
+def create_dataset(positive_images_folder, negative_images_folder):
+    positive_images = load_images_from_folder(positive_images_folder)
+    negative_images = load_images_from_folder(negative_images_folder)
+
+    X = np.array(positive_images + negative_images)
+    y = np.array([1 for _ in range(len(positive_images))] + [0 for _ in range(len(negative_images))])
+    X = X / 255.0
+    y = to_categorical(y, num_classes=2)
+    return X, y
+
+def load_data(type):
+    '''
+        type 0 : RAW
+        type 1 : processed
+        type 2 : external
+    '''
+    if type== 0:
+        root_data_folder = "../data/raw/"
+
+    elif type==1:
+        root_data_folder = "../data/processed/"
+
+    else:
+        NotImplementedError("Code for loading external data not implemented")
+    
+    positive_images_folder = root_data_folder + "/yes/"
+    negative_images_folder = root_data_folder + "/no/"
+
+    return create_dataset(positive_images_folder, negative_images_folder)
+
+
+
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,17 @@
-{\rtf1}
+from data_loader import *
+import matplotlib.pyplot as plt
+
+
+def validate_data():
+    X, y = load_data(type=0)
+
+    print(f"Number of examples: {len(X)} ")
+
+    plt.imshow(X[0])  
+    plt.axis('off')  
+    plt.show()
+
+
+
+if __name__=="__main__":
+    validate_data()
